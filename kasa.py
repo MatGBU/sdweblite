8a2beb9dace673282c9583e69e203b2c6163666f
Mateusz Gorczak <mgorczak@bu.edu>
2024-09-26 12:23:25 -0400

    Finished connection to the strip
    Proper connection with the power strip through the python code, read the comments to know what the code does or refer to the python-casa guide.

--- kasa.py
+++
@@ -1,4 +0,0 @@
-from kasa import Discover, Credentials
-
-found_devices = await Discover.discover()
-[dev.model for dev in found_devices.values()]
--- kasaconnection.py
+++ kasaconnection.py
@@ -2,14 +2,14 @@ import asyncio
 from kasa import Discover
 
 
-async def main():
-    dev = await Discover.discover_single("192.168.0.11",username="gorczam02@gmail.com",password="SeniorDesign28.")
-    await dev.turn_off()
-    await dev.update()
+async def main():    #everything as to run inside the async function
+    dev = await Discover.discover_single("192.168.0.11",username="gorczam02@gmail.com",password="")  #remember to change the password
+    await dev.turn_on()   #turns the device on/off
+    await dev.update()    #updates the firmware
     for feature_name in dev.features:
-     print(feature_name)
-    usage = dev.modules["usage"]
+     print(feature_name)   
+    usage = dev.modules["usage"]    #in theory should display how long something has been plugged into the device, however its not working rn
     print(f"Minutes on this month: {usage.usage_this_month}")
     print(f"Minutes on today: {usage.usage_today}")
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(main())     #run the async function
