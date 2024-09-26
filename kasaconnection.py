7026387c2602f4c3edd32f169d9d93e4e4fae08a
Mateusz Gorczak <mgorczak@bu.edu>
2024-09-25 23:35:15 -0400

    kasa update

--- kasaconnection.py
+++ kasaconnection.py
@@ -2,10 +2,10 @@ import asyncio
 from kasa import Credentials, Discover
 
 async def main():
-    dev = await Discover.discover_single("192.168.0.11",username="mgorczak@bu.edu",password="")  #add password
+    dev = await Discover.discover_single("192.168.0.11",username="mgorczak@bu.edu",password="SeniorDesign28.")  #add password
     await dev.turn_on()
     await dev.update()
-    creds = Credentials("mgorczak@bu.edu", "")  #add password
+    creds = Credentials("mgorczak@bu.edu", "SeniorDesign28.")  #add password
     devices = await Discover.discover(credentials=creds)
     print(len(devices))
 
