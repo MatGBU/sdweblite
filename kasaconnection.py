ddb1c1ea8539e6a84c492453225a0a39b9138341
Mateusz Gorczak <mgorczak@bu.edu>
2024-09-26 00:17:46 -0400

    commit

--- kasaconnection.py
+++ kasaconnection.py
@@ -1,13 +1,15 @@
 import asyncio
-from kasa import Credentials, Discover
+from kasa import Discover
+
 
 async def main():
-    dev = await Discover.discover_single("192.168.0.11",username="mgorczak@bu.edu",password="SeniorDesign28.")  #add password
-    await dev.turn_on()
+    dev = await Discover.discover("192.168.0.11",username="gorczam02@gmail.com",password="SeniorDesign28.")
+    await dev.turn_off()
     await dev.update()
-    creds = Credentials("mgorczak@bu.edu", "SeniorDesign28.")  #add password
-    devices = await Discover.discover(credentials=creds)
-    print(len(devices))
-
+    for feature_name in dev.features:
+     print(feature_name)
+    usage = dev.modules["usage"]
+    print(f"Minutes on this month: {usage.usage_this_month}")
+    print(f"Minutes on today: {usage.usage_today}")
 if __name__ == "__main__":
     asyncio.run(main())
