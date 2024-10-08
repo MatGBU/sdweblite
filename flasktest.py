c7a847617857536f1c3b7cca7199aefd87fa2db6
Mateusz Gorczak <mgorczak@bu.edu>
2024-10-08 13:42:39 -0400

    Changes to web 0.0.1

--- flasktest.py
+++ flasktest.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,4 +8,4 @@ def hello():
     return "Flask is working!"
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)  # Change to a different port, e.g., 5001
+    app.run()  # Change to a different port, e.g., 5001
