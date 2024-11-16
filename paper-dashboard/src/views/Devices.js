8d75043ed68b8ae206f87a1607835b202d8374c3
konstan10 <kostya@bu.edu>
2024-11-16 15:42:19 -0500

    endpoints are hitting

--- paper-dashboard/src/views/Devices.js
+++ paper-dashboard/src/views/Devices.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import axios from "axios"; // Import axios for making HTTP requests
 // reactstrap components
 import {
   Card,
@@ -12,6 +12,30 @@ import {
 } from "reactstrap";
 
 function Devices() {
+  // Function to handle "Turn On" button click
+  const handleTurnOn = () => {
+    axios
+      .get("http://127.0.0.1:8000/turn_on")  // FastAPI backend URL
+      .then((response) => {
+        console.log(response.data.status);  // Log the response message
+      })
+      .catch((error) => {
+        console.error("There was an error turning on the device:", error);
+      });
+  };
+
+  // Function to handle "Turn Off" button click
+  const handleTurnOff = () => {
+    axios
+      .get("http://127.0.0.1:8000/turn_off")  // FastAPI backend URL
+      .then((response) => {
+        console.log(response.data.status);  // Log the response message
+      })
+      .catch((error) => {
+        console.error("There was an error turning off the device:", error);
+      });
+  };
+
   return (
     <>
       <div className="content">
@@ -22,16 +46,15 @@ function Devices() {
                 <CardTitle tag="h4">Kasa Smart Wi-Fi Power Strip</CardTitle>
               </CardHeader>
               <CardBody>
-                <Button color="success" >
+                <Button color="success" onClick={handleTurnOn}>
                   Turn On
                 </Button>
-                <Button color="danger"  className="ml-2">
+                <Button color="danger" className="ml-2" onClick={handleTurnOff}>
                   Turn Off
                 </Button>
               </CardBody>
             </Card>
           </Col>
-          
         </Row>
       </div>
     </>
