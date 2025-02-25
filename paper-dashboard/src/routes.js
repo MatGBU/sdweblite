e31a682c655359395684db9639b449afdd30f3bc
Mateusz Gorczak <mgorczak@bu.edu>
2025-02-25 04:26:22 -0500

    Removing even more components

--- paper-dashboard/src/routes.js
+++ paper-dashboard/src/routes.js
@@ -1,8 +1,6 @@
 
 import Dashboard from "views/Dashboard.js";
-import Notifications from "views/Notifications.js";
-import Devices from "views/Devices.js"; 
-import UserPage from "views/User.js";
+
 
 
 var routes = [
@@ -11,28 +9,6 @@ var routes = [
     name: "Dashboard",
     icon: "nc-icon nc-bank",
     component: <Dashboard />,
-    layout: "/admin",
-  },
-
-  {
-    path: "/notifications",
-    name: "Notifications",
-    icon: "nc-icon nc-bell-55",
-    component: <Notifications />,
-    layout: "/admin",
-  },
-  {
-    path: "/user-page",
-    name: "User Profile",
-    icon: "nc-icon nc-single-02",
-    component: <UserPage />,
-    layout: "/admin",
-  },
-  {
-    path: "/devices",
-    name: "Devices",
-    icon: "nc-icon nc-app",
-    component: <Devices />,
     layout: "/admin",
   },
 
